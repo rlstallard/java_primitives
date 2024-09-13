f143f814c04d807795947f5dd419c563db611923
rlstallard <rlukestallard@gmail.com>
2024-09-13 00:07:34 -0400

    primitives we added during class

---
+++ Primitives/src/main/java/io/github/rlstallard/Primitives.java
@@ -0,0 +1,17 @@
+package io.github.rlstallard;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Primitives {
+    public static void main(String[] args) {
+        int a = 2100000000;
+        short b = 2;
+        long c = 21000000000L;
+        float t = 2.44f;
+        double d = 2.44;
+        char r = 'b';
+        boolean z = true;
+        byte g = 4;
+        }
+    }
+}
